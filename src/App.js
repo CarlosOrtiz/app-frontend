adeae74c60f88b24e3b68bb119862aa84ccda8bb
CarlosOrtiz9901 <carlos.ortiz@udla.edu.co>
2020-04-20 09:03:55 -0500

    feat(app):state and props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+/* function Mycompoment(props) {
+  return (
+    <div id="hello">
+      <h4>{props.subtitle}</h4>
+      {props.text}
+    </div>
+  );
+} */
+
+class Mycompoment extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+  render() {
+    if (this.state.show)
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.subtitle}
+          <button onClick={this.toggleShow}> Toggle show </button>
+        </div>
+      )
+    else
+      return (
+        <div>
+          <br /><b>Nothing Compoment </b>
+          <button onClick={this.toggleShow}>Toggle Show</button>
+        </div>
+      )
+  }
+}
+// const App = () => <div>This us my compoment: <Mycompoment /> </div>  //funcion flecha
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This Is My Compoment:
+      <Mycompoment text='Hello' subtitle='Test 1' />
+      <Mycompoment text='Word' subtitle='Test 1' />
+      <Mycompoment text='z' subtitle='Test 1' error='error' />
     </div>
   );
 }
