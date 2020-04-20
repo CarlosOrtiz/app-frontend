755d194c5c4e4790666552b98f9b7a3bfd521011
CarlosOrtiz9901 <carlos.ortiz@udla.edu.co>
2020-04-20 17:39:01 -0500

    feat(proyect):structure for good practices

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import tasks from '../src/sample/tasks.json'
 
-/* function Mycompoment(props) {
-  return (
-    <div id="hello">
-      <h4>{props.subtitle}</h4>
-      {props.text}
-    </div>
-  );
-} */
-
-class Mycompoment extends React.Component {
+class App extends Component {
 
   state = {
-    show: true
-  }
-
-  toggleShow = () => {
-    this.setState({ show: !this.state.show })
+    tasks: tasks
   }
 
   render() {
-    if (this.state.show)
-      return (
-        <div id="hello">
-          <h3>{this.props.subtitle}</h3>
-          {this.props.subtitle}
-          <button onClick={this.toggleShow}> Toggle show </button>
-        </div>
-      )
-    else
-      return (
-        <div>
-          <br /><b>Nothing Compoment </b>
-          <button onClick={this.toggleShow}>Toggle Show</button>
-        </div>
-      )
-  }
-}
-// const App = () => <div>This us my compoment: <Mycompoment /> </div>  //funcion flecha
-
-function App() {
-  return (
-    <div className="App">
-      This Is My Compoment:
-      <Mycompoment text='Hello' subtitle='Test 1' />
-      <Mycompoment text='Word' subtitle='Test 1' />
-      <Mycompoment text='z' subtitle='Test 1' error='error' />
+    return <div>
+      {this.state.tasks.map( e => <p key={e.id}>
+        {e.id} - {e.title} - {e.detail} - {e.done}
+      </p>
+      )}
     </div>
-  );
+  }
 }
 
 export default App;
